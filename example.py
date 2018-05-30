5832fac226a8065d4c0fc8d0258d4ff03345c272
pgrov4 <39703689+pgrov4@users.noreply.github.com>
2018-05-30 11:45:55 +0530

    Add files via upload

---
+++ example.py
@@ -0,0 +1,29 @@
+#1.print something
+print("hello")
+
+# 3. print variable values
+x=input("enter first no")
+y=input("enter second number")
+z=input("enter third number")
+print("first no is",x)
+print("second no is",y)
+print("third no is",z)
+
+#4. lets get started
+print("""let's" get started""")
+
+# 6. fill in the blanks
+name="tony stark"
+salary=100000
+print('%s %d' % (name, salary))
+
+
+#5. print using placeholders
+print('''s="acadview"''')
+print('''course="python"''')
+print('''fees=5000''')
+
+#2. join strings
+a="acad"
+b="view"
+print(a+b)
